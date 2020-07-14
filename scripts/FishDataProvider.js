351f29b40f92172aa3eaba1801779db54184b706
estherviolin <esthernaomi.violin@gmail.com>
2020-07-14 09:27:01 -0500

    3 fish

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,21 @@
+const fishCollection = [
+    {
+    name: "Sally",
+    species: "swordfish",
+    length: "3 m",
+    location: "Atlantic, Pacific, Indian Oceans",
+    diet: "pelagic fish"
+    },
+    {
+        name: "Zelda",
+        species: "zebrafish",
+        length: "3 cm",
+        location: "South Asia",
+        diet: "plankton, larvae, crustaceans"
+        },
+        name: "",
+        species: "",
+        length: "",
+        location: "",
+        diet: ""
+]
