5436036b52f50a16f94a32bc10a85e0028b2f592
estherviolin <esthernaomi.violin@gmail.com>
2020-07-19 09:33:26 -0500

    removed extra html element

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import {FishList} from "./fish/FishList.js"
 import {LocationList} from "./locations/LocationList.js"
-import {QuoteList} from "./quptes/QuoteList.js"
+import {QuoteList} from "./quotes/QuoteList.js"
 import {TipList} from "./tips/TipList.js"
 
 
